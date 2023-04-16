5f6559fd74bceb4b57459c38661965834f702d5b
mdnumanahmed <mnumansyl@gmail.com>
2023-04-17 03:39:05 +0600

    added context api

--- firebase-auth/src/components/Home/Home.jsx
+++ firebase-auth/src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useSharedData } from '../../Context/context';
 
 const Home = () => {
+    const loadedPhones = useSharedData()
     return (
         <div>
-            <h1>This is Home Page</h1>
+            <h1>Loaded Phones using Context : {loadedPhones.length}</h1>
         </div>
     );
 };
