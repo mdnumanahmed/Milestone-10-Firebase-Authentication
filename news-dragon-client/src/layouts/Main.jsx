90889dc1b9437c497e92c0b5101c4f1f49fa9a23
mdnumanahmed <mnumansyl@gmail.com>
2023-04-24 03:13:04 +0600

    added header and dynamic date using momentjs and added layout

--- news-dragon-client/src/layouts/Main.jsx
+++ news-dragon-client/src/layouts/Main.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import Header from "../pages/Shared/Header";
 import Footer from "../pages/Shared/Footer";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import LeftNav from "../pages/Shared/LeftNav";
+import RightNav from "../pages/Shared/RightNav";
 
 const Main = () => {
   return (
     <div>
       <Header></Header>
+      <Container>
+        <Row>
+          <Col lg={3}>
+            <LeftNav></LeftNav>
+          </Col>
+          <Col lg={6}>
+            Main Content Coming
+          </Col>
+          <Col lg={3}>
+            <RightNav></RightNav>
+          </Col>
+        </Row>
+      </Container>
       <Footer></Footer>
     </div>
   );
--- news-dragon-client/src/pages/Shared/Header.jsx
+++ news-dragon-client/src/pages/Shared/Header.jsx
@@ -1,9 +1,50 @@
 import React from "react";
+import logo from "../../assets/logo.png";
+import moment from "moment";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from "react-bootstrap/Button";
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
   return (
     <div>
-      <h2>This is Header</h2>
+      <Container>
+        <div className="text-center">
+          <img src={logo} alt="" />
+          <p>
+            <small>Journalism Without Fear or Favour</small>
+          </p>
+          <p>{moment().format("dddd, MMMM DD, YYYY")}</p>
+        </div>
+        <div className="bg-light p-2 d-flex">
+          <Button variant="danger">Latest</Button>
+          <Marquee>
+            I can be a React component, multiple React components, or just some
+            text. * I can be a React component, multiple React components, or
+            just some text.
+          </Marquee>
+        </div>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+          <Container>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mx-auto">
+                <Nav.Link href="#features">Home</Nav.Link>
+                <Nav.Link href="#pricing">About</Nav.Link>
+                <Nav.Link href="#career">Career</Nav.Link>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#deets">Profile</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                <Button variant="secondary">Login</Button>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Container>
     </div>
   );
 };
