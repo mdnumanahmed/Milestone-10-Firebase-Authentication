8376ea88fceac2f36a7d85c9b41eb60cfa71a91e
mdnumanahmed <mnumansyl@gmail.com>
2023-04-25 15:38:40 +0600

    server created with news categories api

--- news-dragon-server/index.js
+++ news-dragon-server/index.js
@@ -4,11 +4,32 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.use(cors())
 
 app.get('/', (req, res)=>{
     res.send('News Dragon now published')
+})
+
+app.get('/news', (req, res)=>{
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res)=>{
+    const id = req.params.id
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res)=>{
+    const id = req.params.id
+    if(+id === 0){
+        res.send(news)
+    }else{
+        const categorizedNews = news.filter(c => c.category_id === id)
+        res.send(categorizedNews)
+    }
 })
 
 app.get('/categories', (req, res)=>{
